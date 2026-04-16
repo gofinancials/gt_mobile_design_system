5fa9b8cea216737d1290bad093e9b0450f52d2d0
Mikhaeel Ogunsanya <mikhaeelogunsanya77@gmail.com>
2026-04-16 13:22:49 +0100

    [FEAT]: modifications made to vector illustration icons

---
+++ lib/common/assets/gt_vector_illustration.dart
@@ -0,0 +1,354 @@
+/// A utility class that holds the paths to all vector illustrations (SVGs) used in the application.
+///
+/// This class centralizes asset paths to prevent typos and make them easily accessible
+/// throughout the app.
+class GtVectorIllustrations {
+  static const _basePath = "assets/images/vector/illustrations";
+
+  // region ILLUSTRATIONS
+
+  /// Path to the announcement SVG.
+  static const announcement = "$_basePath/announcement.svg";
+
+  /// Path to the authentication SVG.
+  static const authentication = "$_basePath/authentication.svg";
+
+  /// Path to the bin SVG.
+  static const bin = "$_basePath/bin.svg";
+
+  /// Path to the building SVG.
+  static const building = "$_basePath/building.svg";
+
+  /// Path to the business SVG.
+  static const business = "$_basePath/business.svg";
+
+  /// Path to the camera SVG.
+  static const camera = "$_basePath/camera.svg";
+
+  /// Path to the cash SVG.
+  static const cash = "$_basePath/cash.svg";
+
+  /// Path to the celebration SVG.
+  static const celebration = "$_basePath/celebration.svg";
+
+  /// Path to the chart SVG.
+  static const chart = "$_basePath/chart.svg";
+
+  /// Path to the chat SVG.
+  static const chat = "$_basePath/chat.svg";
+
+  /// Path to the coins SVG.
+  static const coins = "$_basePath/coins.svg";
+
+  /// Path to the currentAccount SVG.
+  static const currentAccount = "$_basePath/current_account.svg";
+
+  /// Path to the dataAnalytics SVG.
+  static const dataAnalytics = "$_basePath/data_analytics.svg";
+
+  /// Path to the date SVG.
+  static const date = "$_basePath/date.svg";
+
+  /// Path to the debitCards SVG.
+  static const debitCards = "$_basePath/debit_cards.svg";
+
+  /// Path to the details SVG.
+  static const details = "$_basePath/details.svg";
+
+  /// Path to the device SVG.
+  static const device = "$_basePath/device.svg";
+
+  /// Path to the disconnected SVG.
+  static const disconnected = "$_basePath/disconnected.svg";
+
+  /// Path to the empty SVG.
+  static const empty = "$_basePath/empty.svg";
+
+  /// Path to the emptyState SVG.
+  static const emptyState = "$_basePath/empty_state.svg";
+
+  /// Path to the exclamation SVG.
+  static const exclamation = "$_basePath/exclamation.svg";
+
+  /// Path to the failed SVG.
+  static const failed = "$_basePath/failed.svg";
+
+  /// Path to the featureUpdate SVG.
+  static const featureUpdate = "$_basePath/feature_update.svg";
+
+  /// Path to the file SVG.
+  static const file = "$_basePath/file.svg";
+
+  /// Path to the flexAccount SVG.
+  static const flexAccount = "$_basePath/flex_account.svg";
+
+  /// Path to the fuel SVG.
+  static const fuel = "$_basePath/fuel.svg";
+
+  /// Path to the gift SVG.
+  static const gift = "$_basePath/gift.svg";
+
+  /// Path to the grow SVG.
+  static const grow = "$_basePath/grow.svg";
+
+  /// Path to the house SVG.
+  static const house = "$_basePath/house.svg";
+
+  /// Path to the identification SVG.
+  static const identification = "$_basePath/identification.svg";
+
+  /// Path to the kids SVG.
+  static const kids = "$_basePath/kids.svg";
+
+  /// Path to the leisure SVG.
+  static const leisure = "$_basePath/leisure.svg";
+
+  /// Path to the location SVG.
+  static const location = "$_basePath/location.svg";
+
+  /// Path to the mail SVG.
+  static const mail = "$_basePath/mail.svg";
+
+  /// Path to the mailbox SVG.
+  static const mailbox = "$_basePath/mailbox.svg";
+
+  /// Path to the maintenance SVG.
+  static const maintenance = "$_basePath/maintenance.svg";
+
+  /// Path to the mobilePhones SVG.
+  static const mobilePhones = "$_basePath/mobile_phones.svg";
+
+  /// Path to the notFound SVG.
+  static const notFound = "$_basePath/not_found.svg";
+
+  /// Path to the notifications SVG.
+  static const notifications = "$_basePath/notifications.svg";
+
+  /// Path to the pair SVG.
+  static const pair = "$_basePath/pair.svg";
+
+  /// Path to the percentage SVG.
+  static const percentage = "$_basePath/percentage.svg";
+
+  /// Path to the politics SVG.
+  static const politics = "$_basePath/politics.svg";
+
+  /// Path to the profile2 SVG.
+  static const profile2 = "$_basePath/profile_2.svg";
+
+  /// Path to the profile3 SVG.
+  static const profile3 = "$_basePath/profile_3.svg";
+
+  /// Path to the profile4 SVG.
+  static const profile4 = "$_basePath/profile_4.svg";
+
+  /// Path to the profileCheck SVG.
+  static const profileCheck = "$_basePath/profile_check.svg";
+
+  /// Path to the referral SVG.
+  static const referral = "$_basePath/referral.svg";
+
+  /// Path to the search SVG.
+  static const search = "$_basePath/search.svg";
+
+  /// Path to the security SVG.
+  static const security = "$_basePath/security.svg";
+
+  /// Path to the selfie SVG.
+  static const selfie = "$_basePath/selfie.svg";
+
+  /// Path to the serviceStatus SVG.
+  static const serviceStatus = "$_basePath/service_status.svg";
+
+  /// Path to the settings SVG.
+  static const settings = "$_basePath/settings.svg";
+
+  /// Path to the sheild SVG.
+  static const sheild = "$_basePath/sheild.svg";
+
+  /// Path to the sheild2 SVG.
+  static const sheild2 = "$_basePath/sheild_2.svg";
+
+  /// Path to the shopping SVG.
+  static const shopping = "$_basePath/shopping.svg";
+
+  /// Path to the signature SVG.
+  static const signature = "$_basePath/signature.svg";
+
+  /// Path to the success SVG.
+  static const success = "$_basePath/success.svg";
+
+  /// Path to the target SVG.
+  static const target = "$_basePath/target.svg";
+
+  /// Path to the tax SVG.
+  static const tax = "$_basePath/tax.svg";
+
+  /// Path to the time SVG.
+  static const time = "$_basePath/time.svg";
+
+  /// Path to the time2 SVG.
+  static const time2 = "$_basePath/time_2.svg";
+
+  /// Path to the unauthorizedDevice SVG.
+  static const unauthorizedDevice = "$_basePath/unauthorized_device.svg";
+
+  /// Path to the vault SVG.
+  static const vault = "$_basePath/vault.svg";
+
+  /// Path to the verification SVG.
+  static const verification = "$_basePath/verification.svg";
+
+  /// Path to the wallet SVG.
+  static const wallet = "$_basePath/wallet.svg";
+
+
+
+
+  // CATEGORY ILLUSTRATIONS
+
+  /// Path to the cashCat SVG.
+  static const cashCat = "$_basePath/cash_cat.svg";
+
+  /// Path to the charityCat SVG.
+  static const charityCat = "$_basePath/charity_cat.svg";
+
+  /// Path to the debitCardsCat SVG.
+  static const debitCardsCat = "$_basePath/debit_cards_cat.svg";
+
+  /// Path to the detailsCat SVG.
+  static const detailsCat = "$_basePath/details_cat.svg";
+
+  /// Path to the educationCat SVG.
+  static const educationCat = "$_basePath/education_cat.svg";
+
+  /// Path to the emergencyCat SVG.
+  static const emergencyCat = "$_basePath/emergency_cat.svg";
+
+  /// Path to the familyCat SVG.
+  static const familyCat = "$_basePath/family_cat.svg";
+
+  /// Path to the fitnessCat SVG.
+  static const fitnessCat = "$_basePath/fitness_cat.svg";
+
+  /// Path to the foodCat SVG.
+  static const foodCat = "$_basePath/food_cat.svg";
+
+  /// Path to the giftCat SVG.
+  static const giftCat = "$_basePath/gift_cat.svg";
+
+  /// Path to the healthCat SVG.
+  static const healthCat = "$_basePath/health_cat.svg";
+
+  /// Path to the holidayCat SVG.
+  static const holidayCat = "$_basePath/holiday_cat.svg";
+
+  /// Path to the houseCat SVG.
+  static const houseCat = "$_basePath/house_cat.svg";
+
+  /// Path to the payrollCat SVG.
+  static const payrollCat = "$_basePath/payroll_cat.svg";
+
+  /// Path to the savingsCat SVG.
+  static const savingsCat = "$_basePath/savings_cat.svg";
+
+  /// Path to the shoppingCat SVG.
+  static const shoppingCat = "$_basePath/shopping_cat.svg";
+
+  /// Path to the transferCat SVG.
+  static const transferCat = "$_basePath/transfer_cat.svg";
+
+  /// Path to the transportCat SVG.
+  static const transportCat = "$_basePath/transport_cat.svg";
+
+  /// Path to the walletCat SVG.
+  static const walletCat = "$_basePath/wallet_cat.svg";
+
+  /// A list containing all available vector illustration SVG paths.
+  static List<String> get all => [
+        announcement,
+        authentication,
+        bin,
+        building,
+        business,
+        camera,
+        cash,
+        celebration,
+        chart,
+        chat,
+        coins,
+        currentAccount,
+        dataAnalytics,
+        date,
+        debitCards,
+        details,
+        device,
+        disconnected,
+        empty,
+        emptyState,
+        exclamation,
+        failed,
+        featureUpdate,
+        file,
+        flexAccount,
+        fuel,
+        gift,
+        grow,
+        house,
+        identification,
+        kids,
+        leisure,
+        location,
+        mail,
+        mailbox,
+        maintenance,
+        mobilePhones,
+        notFound,
+        notifications,
+        pair,
+        percentage,
+        politics,
+        profile2,
+        profile3,
+        profile4,
+        profileCheck,
+        referral,
+        search,
+        security,
+        selfie,
+        serviceStatus,
+        settings,
+        sheild,
+        sheild2,
+        shopping,
+        signature,
+        success,
+        target,
+        tax,
+        time,
+        time2,
+        unauthorizedDevice,
+        vault,
+        verification,
+        wallet,
+        cashCat,
+        charityCat,
+        debitCardsCat,
+        detailsCat,
+        educationCat,
+        emergencyCat,
+        familyCat,
+        fitnessCat,
+        foodCat,
+        giftCat,
+        healthCat,
+        holidayCat,
+        houseCat,
+        payrollCat,
+        savingsCat,
+        shoppingCat,
+        transferCat,
+        transportCat,
+        walletCat,
+      ];
+}
--- lib/widgets/atoms/media/enums/gt_icon_variant.dart
+++ lib/widgets/atoms/media/enums/gt_icon_variant.dart
@@ -6,10 +6,10 @@ enum GtIconVariant { strong, sub, soft, disabled, white }
 extension GtIconVariantExtension on GtIconVariant? {
   Color getIconColor(BuildContext context) {
     return switch (this) {
-      GtIconVariant.sub => context.palette.icon.sub,
-      GtIconVariant.soft => context.palette.icon.soft,
-      GtIconVariant.disabled => context.palette.icon.disabled,
-      GtIconVariant.white => context.palette.icon.white,
+      .sub => context.palette.icon.sub,
+      .soft => context.palette.icon.soft,
+      .disabled => context.palette.icon.disabled,
+      .white => context.palette.icon.white,
       _ => context.palette.icon.strong,
     };
   }
