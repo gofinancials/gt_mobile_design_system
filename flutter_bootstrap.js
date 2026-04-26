a4e5a7db8ebb7b66568186188de419054b1664b0
Mikhaael <100805785+Mikhaael@users.noreply.github.com>
2026-04-26 14:58:43 +0000

    Deploying to gh-pages from @ gofinancials/gt_mobile_design_system@bb2d37e138fdca76eebb56a8ca0f285e35249da0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1598931067" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "653223154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
