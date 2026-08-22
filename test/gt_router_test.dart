cbe79a129e1dc8a8ce7fcaf602c8b76ca9c5b3b6
Jesse Dirisu <dirisujesse@gmail.com>
2026-08-22 20:54:14 +0100

    refactor: update GtBottomSheet API to use scrollableBuilder, improve search input interaction, and refine project configuration

--- test/gt_router_test.dart
+++ test/gt_router_test.dart
@@ -173,7 +173,7 @@ void main() {
       unawaited(
         showCupertinoSheet<void>(
           context: GtRouter.observer.navigator!.context,
-          builder: (_) => const SizedBox(height: 100),
+          scrollableBuilder: (context, _) => const SizedBox(height: 100),
         ),
       );
       await tester.pumpAndSettle();
